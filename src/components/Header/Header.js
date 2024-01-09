422ec7295d575e0dc28fef66f4a4ccc7bd9af7f9
Saimul-Shanto <saimulhaque@iut-dhaka.edu>
2024-01-09 12:59:16 +0600

    create header component and create movie.json file

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Buy your favorite movie ticket</h2>
+        </div>
+    );
+};
+
+export default Header;
